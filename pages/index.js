4c6d6f31a28687935ce9e2e17fa4ac1b70315a20
Muhammetnur Amandurdyev <verf631664@gmail.com>
2021-10-06 11:32:54 +0500

    Responsive, SEO, quotes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from '../styles/mainPage.module.css';
 import { gsap } from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
@@ -98,8 +98,20 @@ function shuffle (array){
 }
 
 export default function MainPage(props){
-  const [ready, setReady] = useState(false);
-  const [alert, setAlert] = useState(false);
+  const [ready, _setReady] = useState(false);
+  const [alert, _setAlert] = useState(false);
+  const alertRef = useRef(alert);
+  const readyRef = useRef(ready);
+
+  const setReady = (value)=>{
+    readyRef.current = value;
+    _setReady(value);
+  }
+
+  const setAlert = (value)=>{
+    alertRef.current=value;
+    _setAlert(value);
+  } 
 
   function applyAnimation(){
     let section1=document.getElementsByClassName('section')[0];
@@ -107,6 +119,12 @@ export default function MainPage(props){
     let section3=document.getElementsByClassName('section')[2];
     let section4=document.getElementsByClassName('section')[3];
     let spans = document.querySelectorAll(".section:nth-child(1) > div > span");
+
+    window.mobileCheck = function() {
+      let check = false;
+      (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+      return check;
+    };
 
     gsap.registerPlugin(ScrollTrigger);
 
@@ -121,7 +139,7 @@ export default function MainPage(props){
         trigger: section1,
         pin: true,
         markers: false,
-        end: '+=' + ((cv.length+4)*section2.offsetWidth),
+        end: mobileCheck() ? '+=' + ((cv.length+6)*section2.offsetWidth): '+=' + ((cv.length+4)*section2.offsetWidth),
       }
     })
     // let array = Array.from(spans).reverse();
@@ -171,11 +189,26 @@ export default function MainPage(props){
     tl2.to(section2, {yPercent: -100, opacity: 0, ease: 'none', duration: 0})
 
     // section 3 🎃🎡
-    let quoteWords = document.querySelector('#quote').childNodes;
-    let shuffled = shuffle(Array.from(quoteWords).filter((el)=> el.tagName==='SPAN'))
+    
+    let quoteWords1 = document.querySelectorAll('.quote')[0].childNodes;
+    let shuffled1 = shuffle(Array.from(quoteWords1).filter((el)=> el.tagName==='SPAN'))
 
-    for(let i=0; i<shuffled.length; i++){
-      tl2.from(shuffled[i], {opacity: 0, duration: 6}, '<+0.5')
+    let quoteWords2 = document.querySelectorAll('.quote')[1].childNodes;
+    let shuffled2 = shuffle(Array.from(quoteWords2).filter((el)=> el.tagName==='SPAN'))
+
+    for(let i=0; i<shuffled1.length; i++){
+      tl2.from(shuffled1[i], {opacity: 0, duration: 6}, '<+0.5')
+    }
+    
+    // just delay fading first quote 
+    tl2.to(shuffled1, {delay: 3})
+
+    for(let i=0; i<shuffled1.length; i++){
+      tl2.to(shuffled1[i], {opacity: 0, duration: 6},'<+0.5')
+    }
+
+    for(let i=0; i<shuffled2.length; i++){
+      tl2.from(shuffled2[i], {opacity: 0, duration: 6}, '<+0.5')
     }
 
     // 1️⃣
@@ -207,47 +240,57 @@ export default function MainPage(props){
     // section 4 🎇🧨💄🎯🏆
     tl2.from(section4, {height: 0,width:0, duration: 24, ease: 'none'}, '>-8');
     tl2.to('#circularTextWrapper', {rotation: -360, duration: 24, ease: 'none'}, '<').to('#circularTextWrapper', {opacity: 0, duration: 5, zIndex: -1}, '>-5');
-    
+
     let trainText = document.querySelector('#trainText'); 
 
     tl2.to(trainText, {x: `-${(window.visualViewport.width+trainText.offsetWidth)}px`, duration: trainText.children.length, ease: 'none'});
 
-    document.querySelectorAll('#trainText span').forEach((node, index)=>{
-      let random = Math.random();
-      let randomValue = Math.floor(random*30);
-      // let up = random > 0.5 ? true : false
-      let up = index % 2
+    if(mobileCheck()){
 
-      if(index===0){
-        tl2.from(node, {transform: `translateY(${up ? '': '-'}${randomValue}vh) scale(0.5)`, duration: 5, ease: 'back.inOut(3)'}, '<+=0.5')
-      } else{
-        tl2.from(node, {transform: `translateY(${up ? '': '-'}${randomValue}vh) scale(0.5)`, duration: 5, ease: 'back.inOut(3)'}, '<+0.76')
-      }
-    })
+    } else{
+      document.querySelectorAll('#trainText span').forEach((node, index)=>{
+        let random = Math.random();
+        let randomValue = Math.floor(random*30);
+        // let up = random > 0.5 ? true : false
+        let up = index % 2
+  
+        if(index===0){
+          tl2.from(node, {transform: `translateY(${up ? '': '-'}${randomValue}vh) scale(0.5)`, duration: 5, ease: 'back.inOut(3)'}, '<+=0.5')
+        } else{
+          tl2.from(node, {transform: `translateY(${up ? '': '-'}${randomValue}vh) scale(0.5)`, duration: 5, ease: 'back.inOut(3)'}, '<+0.76')
+        }
+      })
+    }
 
     tl2.from('#contacts', {yPercent: 200, duration: 30, ease: 'none'}, '>')
 
     setReady(true)
   }
 
-  useEffect(()=>{
-    let prevHeight = 0;
+  function validateProportion(){
+   return window.innerWidth/window.innerHeight>1.6
+  }
 
-    window.addEventListener('resize', (e)=>{
-
-      if(prevHeight<e.currentTarget.innerHeight){
-        
-      } else{
-        window.location.reload(false);
-      }
-      prevHeight = e.currentTarget.innerHeight;
-
-    })
-    let invalidProportion = window.innerWidth/window.innerHeight<1.6;
-
-    if(invalidProportion){
-      setAlert(true);
+  function handleResize(e){
+    if(validateProportion()){
+      alertRef.current && setAlert(false);
+      readyRef.current || applyAnimation();
     } else{
+      alertRef.current || setAlert(true);
+    }
+  }
+
+  useEffect(()=>{
+    let time;
+
+    window.addEventListener('resize', (e)=> {
+      if(time){clearTimeout(time)};
+      time = setTimeout(()=>handleResize(e),100)
+    })
+    
+    if(!validateProportion()){
+      setAlert(true);
+    }else{
       applyAnimation();
     }
 
@@ -255,13 +298,19 @@ export default function MainPage(props){
 
   return(
     <div>
-      {alert ? 
-      <div style={{width:'100vw', height: '100vh', fontSize: '5vw', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      {alert &&
+      <div style={{width:'100vw', height: '100vh', position: 'fixed', left: 0, top: 0,fontSize: '5vw', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
         Rotate your browser and continue
-      </div>:
-      <div style={!ready ? {opacity: '0'}: {}}>
+      </div>}
+
+      <div style={(alert || !ready) ? {opacity: 0}: {}}>
         <Head>
           <title>Muhammetnur Amandurdyev</title>
+          <meta name='descriptions' content='CV / Resume of turkmen programmer and engineer'/>
+          <meta name='robots' content='index, follow'/>
+          <meta name='author' content='Muhammetnur Amandurdyev'/>
+          <meta name='keywords' content='turkmen, Muhammetnur, Amandurdyev, Amandurdyyev, Amandurdyýew, Амандурдыев, Мухамметнур, cv, resume'/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         </Head>
 
           <section id={styles.section1} className='section'>
@@ -405,19 +454,47 @@ export default function MainPage(props){
           <section id={styles.section3} className='section'> 
             <section>
               <div className={`${styles.layer} layer`}>
-                <p id='quote'>
-                  <span>&nbsp;“Here</span> 
-                  <span>&nbsp;must</span> 
-                  <span>&nbsp;be</span> 
-                  <span>&nbsp;very</span> 
-                  <span>&nbsp;smart</span> 
-                  <span>&nbsp;and</span> 
-                  <span>&nbsp;sharp</span> 
-                  <span>&nbsp;words”</span> 
-                  <br/><br/>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                  <span>&#8212;&nbsp;Frederic</span>
-                  <span>&nbsp;Schopengauer</span>
+
+                <p className={`${styles.quote} quote`}>
+                  <span>&nbsp;&nbsp;“Muhammetnur</span> 
+                  <span>&nbsp;has</span> 
+                  <span>&nbsp;been</span> 
+                  <span>&nbsp;one</span> 
+                  <span>&nbsp;off</span> 
+                  <span>&nbsp;the</span> 
+                  <span>&nbsp;most</span> 
+                  <br/>
+                  <span>&nbsp;productive</span> 
+                  <span>&nbsp;students</span> 
+                  <span>&nbsp;in</span> 
+                  <span>&nbsp;our</span> 
+                  <span>&nbsp;drilling</span> 
+                  <span>&nbsp;department</span>
+                  <br/>
+                  <span>&nbsp;studied</span> 
+                  <span>&nbsp;during my</span> 
+                  <span>&nbsp;5 year tenure</span> 
+                  <span>&nbsp;as docent”</span> 
+                  <br/><br/>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+                  <span> &#8212; &nbsp;Ekaterine Leusheva</span>
+                  <span>, Docent</span>
                 </p>
+
+                <p className={`${styles.quote} quote`}>
+                  <span>&nbsp;“He</span> 
+                  <span>&nbsp;is</span> 
+                  <span>&nbsp;talented</span> 
+                  <span>&nbsp;young</span> 
+                  <span>&nbsp;man</span> 
+                  <span>&nbsp;driven</span> 
+                  <span>&nbsp;by passion</span> 
+                  <span>&nbsp;for engineering</span> 
+                  <span>&nbsp;and drilling”</span> 
+                  <br/><br/>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                  <span>&#8212;&nbsp;Drilling</span>
+                  <span>&nbsp;Superintendent</span>
+                </p>
+
               </div>
               <div className={`${styles.layer} layer`}></div>
 
@@ -600,7 +677,7 @@ export default function MainPage(props){
             </div>
           </section>
 
-      </div>}
+      </div>
     </div>
   )
 }
